c7944b40ea2c6ef6b05ecfbb2eb55b67628b765f
DivyaPallerla <83897390+DivyaPallerla@users.noreply.github.com>
2025-04-16 00:34:41 +0000

    lab4

---
+++ products.js
@@ -0,0 +1,48 @@
+const fs = require('fs').promises
+const path = require('path')
+
+const productsFile = path.join(__dirname, 'data/full-products.json')
+
+/**
+ * List all products
+ * @param {Object} options
+ * @returns {Promise<Array>}
+ */
+async function list(options = {}) {
+  const { offset = 0, limit = 25, tag } = options
+  const data = await fs.readFile(productsFile)
+  let products = JSON.parse(data)
+  
+  // Filter by tag if provided
+  if (tag) {
+    products = products.filter(product => 
+      product.tags && product.tags.some(t => t.title === tag)
+    )
+  }
+
+  return products.slice(offset, offset + limit) // Slice the products
+}
+
+/**
+ * Get a single product
+ * @param {string} id
+ * @returns {Promise<object>}
+ */
+async function get(id) {
+  const products = JSON.parse(await fs.readFile(productsFile))
+
+  // Loop through the products and return the product with the matching id
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      return products[i]
+    }
+  }
+
+  // If no product is found, return null
+  return null
+}
+
+module.exports = {
+  list,
+  get
+}
